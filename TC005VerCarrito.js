d6696f9250b237980f9a6dcd2397aa4a5e94bf56
iampedroluis <83560861+iampedroluis@users.noreply.github.com>
2021-05-03 00:48:06 -0300

    Delete TC005VerCarrito.js

--- TC005VerCarrito.js
+++
@@ -1,7 +0,0 @@
-describe('Ver Carrito Home', function(){
-    it('TC-Amazon-Ver Carrito Home', function(){
-        cy.visit('https://www.amazon.com/')
-        cy.get('#nav-cart-count').click();
-
-    })
-})
