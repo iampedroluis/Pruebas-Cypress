459b13176c10f3ad2cb2f5f82e34558382be3b75
iampedroluis <83560861+iampedroluis@users.noreply.github.com>
2021-05-03 00:52:10 -0300

    Delete SliderGaleriaHome.js

--- cypress/integration/SliderGaleriaHome.js
+++
@@ -1,7 +0,0 @@
-describe('Slider Galeria Home', function(){
-    it('TC-Amazon-Slider Galeria Home', function(){
-        cy.visit('https://www.amazon.com/')
-        cy.get('.a-carousel-goto-nextpage').click();
-
-    })
-})
